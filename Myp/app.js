f8f549a63ff9023cbcbd61a46f9436bbe1311347
Abdirahman <90302779+Iammaankah@users.noreply.github.com>
2023-01-04 03:26:27 +0530

    Update app.js

--- Myp/app.js
+++ Myp/app.js
@@ -16,33 +16,3 @@ burger.addEventListener('click', () => {
 //   // document.body.style.background = "#FF70FD"
 // });
  
-// let time = 20;
-// (time < 18) ? console.log("Good day.") : console.log("Good evening.");
-
-
-
-
-
-
-
-/// calling http request
-
-let request = new XMLHttpRequest;
-// console.log(request.readyState)
-request.addEventListener('readystatechange',()=>{
-  if (request.readyState === 4 && request.status== 200){
-    console.log(request.responseText)
-  }else{
-    console.log('somthing wrong..')
-  }
-})
-
-
-request.open('GET','https://jsonplaceholder.typicodee.com/posts/1');
-
-request.send()
-//  console.log(request.readyState)
-//  console.log(request)
-  
-
-
